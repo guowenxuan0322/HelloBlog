e9238da717a98e0e33979d4d4646e9ce0c5190d3
gwx <guowenxuan0322@163.com>
2018-02-25 22:35:19 +0800

    增加
    print no

--- src/main/java/net/evercodig/helloblog/HelloBlogApplication.java
+++ src/main/java/net/evercodig/helloblog/HelloBlogApplication.java
@@ -3,6 +3,7 @@ package net.evercodig.helloblog;
 public class HelloBlogApplication {
     public static void main(String[] args) {
         System.out.println("ok");
+        System.out.println("no");
     }
 
 }
