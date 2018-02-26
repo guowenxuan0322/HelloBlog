0f3b6be559d59fdbde66d5fa0fa738b4720f05de
gwx <guowenxuan0322@163.com>
2018-02-26 22:26:24 +0800

    二次提交writings.sql

--- sql/writings.sql
+++ sql/writings.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50717
 File Encoding         : 65001
 
-Date: 2018-02-26 22:08:19
+Date: 2018-02-26 22:24:52
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -21,9 +21,9 @@ SET FOREIGN_KEY_CHECKS=0;
 DROP TABLE IF EXISTS `writings`;
 CREATE TABLE `writings` (
   `id` int(11) DEFAULT NULL,
-  `heading` varchar(255) DEFAULT NULL,
-  `content` varchar(255) DEFAULT NULL,
-  `author` varchar(255) DEFAULT NULL,
-  `inittime` datetime DEFAULT NULL,
-  `changetime` datetime DEFAULT NULL
+  `heading` varchar(255) NOT NULL,
+  `content` varchar(255) NOT NULL,
+  `author` varchar(255) NOT NULL,
+  `inittime` bigint(20) NOT NULL,
+  `changetime` bigint(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
