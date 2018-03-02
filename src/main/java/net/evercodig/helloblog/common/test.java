59a84e7c68e938ba0fd0d6f141afbe1d26c65526
gwx <guowenxuan0322@163.com>
2018-03-02 18:09:57 +0800

    logger 提交

--- src/main/java/net/evercodig/helloblog/common/test.java
+++ src/main/java/net/evercodig/helloblog/common/test.java
@@ -1,13 +1,17 @@
 package net.evercodig.helloblog.common;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Random;
+
+import org.apache.log4j.Logger;
 
 public class test {
+    private static Logger logger = Logger.getLogger(test.class);
     public static void main(String[] args) {
-        HashMap<String, String> map = new HashMap<>();
-        String md5 = MD5.getMD5(new Random().toString());
-        System.out.println(md5);
+            // 记录debug级别的信息
+            logger.debug("This is debug message.");
+            // 记录info级别的信息
+            logger.info("This is info message.");
+            // 记录error级别的信息
+            logger.error("This is error message.");
+
     }
 }
--- src/main/java/net/evercodig/helloblog/controller/LoginController.java
+++ src/main/java/net/evercodig/helloblog/controller/LoginController.java
@@ -2,6 +2,8 @@ package net.evercodig.helloblog.controller;
 
 import net.evercodig.helloblog.pojo.UserVO;
 import net.evercodig.helloblog.service.LoginService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,13 +13,15 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/login")
 public class LoginController {
-
+    private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
     @Autowired
     LoginService loginService;
 
     @RequestMapping(method = RequestMethod.POST)
     public String login(@RequestBody UserVO userVO){
         String result = loginService.compare(userVO);
+        logger.info("结果{}", result);
         return result;
+
     }
 }
